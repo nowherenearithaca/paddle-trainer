9fd5ca964d575b2c43cf3054929b3cba8de93b37
nowherenearithaca <bradflyon@gmail.com>
2025-09-29 11:18:17 -0400

    tweak for export

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,13 @@
-export default { reactStrictMode: true }
+// export default { reactStrictMode: true }
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export", // <-- enables static export
+  trailingSlash: true, // <-- good for basic hosts (serves /page/index.html)
+  // If deploying under a subfolder like https://yourdomain.com/paddle
+  // basePath: '/paddle',   // <-- uncomment & set, then re-export
+  basePath: "/paddle-trainer",
+  assetPrefix: "/paddle-trainer/",
+};
+export default nextConfig;
